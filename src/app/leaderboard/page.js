eff4598729d83773cbed4b37f75571e2872fd45c
Aaru5h <mestrikerocks@gmail.com>
2025-05-06 12:47:44 +0530

    added Leaderboaard UI

--- src/app/leaderboard/page.js
+++ src/app/leaderboard/page.js
@@ -1,8 +1,59 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
+import './styles.css'
 
 const Leaderboard = () => {
+
+  const [leaderboardData, setLeaderBoardData] = useState([])
+  const [load,setLoad] = useState(true)
+
+  useEffect(()=>{
+    const fetchUserLeaderboard = ()=>{
+
+      setTimeout(()=>{
+        setLoad(true)
+        setLeaderBoardData([
+          { name: 'John Doe', workouts: 12 },
+          { name: 'Jane Smith', workouts: 10 },
+          { name: 'Jim Brown', workouts: 8 },
+        ]);
+        setLoad(false)
+      },1000)
+    }
+
+    fetchUserLeaderboard()
+  },[])
+
   return (
-    <div>Leaderboard Here</div>
+    <div className="leaderboard-container">
+      <h1 className="leaderboard-title">Leaderboard</h1>
+
+      {
+        load ? <p className="loading-message">Loading...</p>
+        :
+        (
+          <table className="leaderboard-table">
+            <thead>
+              <tr>
+                <th>Position</th>
+                <th>Name</th>
+                <th>Workouts Completed</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {leaderboardData.map((user, index) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{user.name}</td>
+                  <td>{user.workouts}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )
+      }
+    </div>
   )
 }
 
